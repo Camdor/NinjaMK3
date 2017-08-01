d5736995ec48619c5f98241f6d13ae76290f0bb9
Tobias Keijser <kt.keijser@gmail.com>
2017-08-01 10:04:38 +0200

    Project Setup

---
+++ MiniNinja/MiniNinja/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MiniNinja.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MiniNinja/MiniNinja/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiniNinja.WebApiApplication" Language="C#" %>
